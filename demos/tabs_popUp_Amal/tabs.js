f47e65b8a7169625a5ba47946fd470a1e3595cf0
Amal Kaduwela <akaduwel@ucsd.edu>
2021-05-10 19:37:52 -0700

    Added dropdown demo

--- demos/tabs_popUp_Amal/tabs.js
+++ demos/tabs_popUp_Amal/tabs.js
@@ -31,3 +31,22 @@ function addDay(evt) {
   ul.appendChild(li);
 
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openDropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.ddlinks')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
