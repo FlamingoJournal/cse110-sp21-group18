e80aa8969aa887d5519a6b0cd187aafa62d821c3
Angel-MtzV <anm002@ucsd.edu>
2021-05-11 17:49:09 -0700

    Sidebar

---
+++ demos/sidebar_collapse/script.js
@@ -0,0 +1,36 @@
+function classSwitch(id, oldClass, newClass){
+    let element = id;
+    element.classList.remove(oldClass);
+    element.classList.add(newClass);
+}
+
+document.getElementById("menu").onclick = function(){    
+    if (this.value=="<") this.value = ">";
+    else this.value = "<";
+}
+
+document.getElementById("menu").onclick = function(){
+    let sidebar = document.getElementById("sidebar");
+    let img = document.getElementById("calendar");
+    let font = document.getElementById("font");
+    let menu = document.getElementById("menu");
+    let log = document.getElementById("log");
+
+    if(sidebar.className === "sidebaron"){
+        this.value = ">";
+        img.src = "";
+        font.src = "";
+        classSwitch(sidebar, "sidebaron", "sidebaroff");
+        classSwitch(menu, "collapse", "open");
+        classSwitch(log, "logSideon", "logSideoff");
+    }
+
+    else{
+        this.value = "<";
+        img.src = "calendar.png";
+        font.src = "Font.png";
+        classSwitch(sidebar, "sidebaroff", "sidebaron");
+        classSwitch(menu, "open", "collapse");
+        classSwitch(log, "logSideoff", "logSideon");
+    }
+}
